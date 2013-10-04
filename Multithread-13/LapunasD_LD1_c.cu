cc142be4031f820fd026c7fab46ecba294860968
DariusL <d.lapunas@gmail.com>
2013-10-04 17:05:50 +0300

    comments

--- Multithread-13/LapunasD_LD1_c.cu
+++ Multithread-13/LapunasD_LD1_c.cu
@@ -1,4 +1,7 @@
-
+/*
+	Procesas padaro atsitiktini skaiciu iteraciju
+	Atsitiktine
+*/
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
@@ -15,6 +18,7 @@
 
 using namespace std;
 
+//struktura, kuri bus kopijuojama i GPU su rodykle i teksto eilute
 struct GpuStruct
 {
 	char *pav;
@@ -22,6 +26,7 @@ struct GpuStruct
 	double kaina;
 };
 
+//pagrindine struktura, kuri bus sukuriama skaitant duomenis
 class Struct
 {
 	string pav;
@@ -30,11 +35,12 @@ class Struct
 	GpuStruct gpuStruct;
 public:
 	Struct(string input);
-	~Struct(){cudaFree(gpuStruct.pav);}
+	~Struct(){cudaFree(gpuStruct.pav);}//destruktoriuje istrinama teksto eilute is GPU
 	GpuStruct GetDev(){return gpuStruct;}
 	string Print();
 };
 
+//is duomenu eilutes sukuriama struktura, sukuriama struktura kuri bus GPU, alokuojama teksto eilute GPU
 Struct::Struct(string input)
 {
 	int start, end;
@@ -72,6 +78,7 @@ int main()
 {
 	auto input = ReadStuff("LapunasD.txt");
 	int count = 0;
+	//suskaiciuojama kiek is viso yra duomenu
 	for(auto &vec : input)
 		count += vec.size();
 	cout << "\nsinchroninis isvedimas\n\n";
@@ -79,12 +86,15 @@ int main()
 	cout << "\nasinchroninis isvedimas\n\n";
 	cout << setw(10) << "Procesas" << setw(3) << "Nr" << Titles() << "\n\n";
 	
+	//procesu duomenu pradzios indeksai
 	vector<int> starts;
+	//lokalios GPU strukturu kopijos
 	vector<GpuStruct> localStructs;
 	
 	int put = 0;
 	for(auto &vec : input)
 	{
+		//proceso pradzia
 		starts.push_back(put);
 		for(auto &s : vec)
 		{
@@ -94,26 +104,36 @@ int main()
 	}
 	starts.push_back(put);
 	int *startsdev;
+	//pradziu masyvas GPU
 	cudaMalloc(&startsdev, sizeof(int) * starts.size());
 	cudaMemcpy(startsdev, &starts[0], sizeof(int) * starts.size(), cudaMemcpyHostToDevice);
 	GpuStruct *arr;
+	//strukturu masyvas GPU
 	cudaMalloc(&arr, sizeof(GpuStruct) * count);
 	cudaMemcpy(arr, &localStructs[0], sizeof(GpuStruct) * count, cudaMemcpyHostToDevice);
+	//GPU funkcija
 	DevPrint<<<1, input.size()>>>(arr, startsdev);
+	//palaukiam kol gpu baigs spausdint, "pause" uzrakina konsole
 	cudaDeviceSynchronize();
 	system("pause");
+	//atlaisvinami pagrindiniai masyvai, teksto eilutes atlaisvinamos sunaikintant pagrindines strukturas - input
 	cudaFree(arr);
 	cudaFree(startsdev);
 	return 0;
 }
 
+//is failo skaitomi duomenis i strukturu matrica
 vector<vector<Struct>> ReadStuff(string file)
 {
+	//eiluciu vektorius
 	auto lines = ReadLines(file);
+	//duomenu matrica
 	vector<vector<Struct>> ret;
+	//laikinas vektorius vieno proceso duomenim
 	vector<Struct> tmp;
 	for(int i = 0; i < lines.size(); i++)
 	{
+		//tuscia eilute skiria procesus
 		if(lines[i] == "")
 		{
 			ret.push_back(move(tmp));
@@ -126,6 +146,7 @@ vector<vector<Struct>> ReadStuff(string file)
 	return ret;
 }
 
+//failas skaitomas i eiluciu vektoriu
 vector<string> ReadLines(string file)
 {
 	vector<string> ret;
@@ -146,6 +167,7 @@ string Titles()
 	return ss.str();
 }
 
+//grazus sinchroninis spausdinimas
 void syncOut(vector<vector<Struct>> &data)
 {
 	cout << setw(3) << "Nr" << Titles() << endl << endl;
@@ -167,11 +189,13 @@ string Print(int nr, Struct &s)
 	return ss.str();
 }
 
+//GPU funkcija spausdinimui
+//gauna duomenis ir indeksus kur prasideda kieno duomenys
 __global__ void DevPrint(GpuStruct *data, int *starts)
 {
 	int id = threadIdx.x;
-	GpuStruct *d = data + starts[id];
-	int count = starts[id+1] - starts[id];
+	GpuStruct *d = data + starts[id];//proceso duomenu pradzia
+	int count = starts[id+1] - starts[id];//proceso duomenu kiekis
 	for(int i = 0; i < count; i++)
 	{
 		printf("Procesas_%i %2i %14s %6i %19f\n", id, i, d[i].pav, d[i].kiekis, d[i].kaina);
